913bf337a44a9630cd0b0677119b057bc007632a
BenZ-H <bz028@foxmail.com>
2022-05-20 18:05:31 +0800

    use ipfsAPI old version

--- packages/hardhat/scripts/mint2.js
+++ packages/hardhat/scripts/mint2.js
@@ -5,7 +5,7 @@ const { config, ethers } = require("hardhat");
 const { utils } = require("ethers");
 const R = require("ramda");
 const ipfsAPI = require('ipfs-http-client');
-const ipfs = ipfsAPI.create({host: 'ipfs.infura.io', port: '5001', protocol: 'https' })
+const ipfs = ipfsAPI({host: 'ipfs.infura.io', port: '5001', protocol: 'https' })
 
 const delayMS = 1000 //sometimes xDAI needs a 6000ms break lol 😅
 
